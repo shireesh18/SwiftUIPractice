af26d9e83fc62de7338dad27892bfd92c1cb1e72
Shireesh <shireesh18@gmail.com>
2024-06-15 08:22:06 +0100

    Initial Commit

---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Shireesh Marla on 15/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
